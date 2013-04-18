544351609081fec64f5e6f17153be1baf292df3d
lorus <lore@lorus.org>
2013-04-18 15:42:35 +0200

    fixed typo in repo xml

--- repository/addons_gen.cmd
+++ repository/addons_gen.cmd
@@ -1,5 +1,9 @@
 copy ..\addons\repository.addons.siriuz\addon.xml repository.addons.siriuz
+copy ..\addons\repository.addons.siriuz\icon.png repository.addons.siriuz
+
 copy ..\addons\script.module.lxml-win32\addon.xml script.module.lxml-win32
+
 copy ..\addons\plugin.video.teamstream-win32\addon.xml plugin.video.teamstream-win32
+copy ..\addons\plugin.video.teamstream-win32\icon.png plugin.video.teamstream-win32
 
 "C:\Program Files (x86)\Python-2.6.6\python.exe" addons_gen.py
