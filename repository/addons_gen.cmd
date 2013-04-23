13d4148f0788c9ea50fc09014aa09af9e1e45559
lorus <lore@lorus.org>
2013-04-23 02:12:32 +0200

    repository cleanup

--- repository/addons_gen.cmd
+++ repository/addons_gen.cmd
@@ -1,9 +1,9 @@
 copy ..\addons\repository.addons.siriuz\addon.xml repository.addons.siriuz
 copy ..\addons\repository.addons.siriuz\icon.png repository.addons.siriuz
 
-copy ..\addons\script.module.lxml\addon.xml script.module.lxml
 
 copy ..\addons\plugin.video.teamstream\addon.xml plugin.video.teamstream
 copy ..\addons\plugin.video.teamstream\icon.png plugin.video.teamstream
+copy ..\addons\plugin.video.teamstream\changelog.txt plugin.video.teamstream
 
 "C:\Program Files (x86)\Python-2.6.6\python.exe" addons_gen.py
