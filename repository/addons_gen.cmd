d4a44fc9b465b8893c3c9a7aa3382a8c08107da0
lorus <lore@lorus.org>
2013-04-23 03:01:56 +0200

    adding script.module.lxml-win32 as optional package

--- repository/addons_gen.cmd
+++ repository/addons_gen.cmd
@@ -1,6 +1,7 @@
 copy ..\addons\repository.addons.siriuz\addon.xml repository.addons.siriuz
 copy ..\addons\repository.addons.siriuz\icon.png repository.addons.siriuz
 
+copy ..\addons\script.module.lxml-win32\addon.xml script.module.lxml-win32
 
 copy ..\addons\plugin.video.teamstream\addon.xml plugin.video.teamstream
 copy ..\addons\plugin.video.teamstream\icon.png plugin.video.teamstream
