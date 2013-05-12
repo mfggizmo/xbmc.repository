442961e4668481039efb3ced56f0e0354f15e8a3
lore_deluxe <lore@lorus.org>
2013-05-12 14:23:36 +0200

    add BeautifulSoup to repository

--- repository/addons_gen.cmd
+++ repository/addons_gen.cmd
@@ -5,4 +5,6 @@ copy ..\addons\plugin.video.teamstream\addon.xml plugin.video.teamstream
 copy ..\addons\plugin.video.teamstream\icon.png plugin.video.teamstream
 copy ..\addons\plugin.video.teamstream\changelog.txt plugin.video.teamstream
 
+copy ..\addons\script.module.beautifulsoup\addon.xml script.module.beautifulsoup
+
 "C:\Program Files (x86)\Python-2.6.6\python.exe" addons_gen.py
